05c64f2a71c90e0aac392f78c376225cd72e7274
midoliy <midoliy@outlook.jp>
2023-10-25 16:18:53 +0900

    wip

---
+++ Owl.mht/src/psr2html/Program.fs
@@ -0,0 +1,17 @@
+﻿open Owl.mht
+
+let args = System.Environment.GetCommandLineArgs()
+printfn "%A" args
+
+try
+  let mht = Mht.fpath args[1]
+  let pages = mht |> Mht.load |> Seq.map Mime.parse
+  for page in pages do
+    match page with
+    | Mime.text (content, encode) -> ()
+    | Mime.image (content, encode) -> ()
+    | _ -> ()
+
+
+with
+  e -> printfn "%s" e.Message

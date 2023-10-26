cec03f37aaf77f8b7a3cc45d01cb142dfffc2074
midoliy <midoliy@outlook.jp>
2023-10-26 17:51:38 +0900

    wip

--- Owl.mht/src/Owl.mht/MhtSonic.fs
+++ Owl.mht/src/Owl.mht/MhtSonic.fs
@@ -31,14 +31,36 @@ module Mht =
 
   let get_ctencode (lines: string[]) =
     let rec f i =
-      if lines.Length <= i then raise (exn "ContentTransferEncode is not found")
-      let line = lines[i]
-      match Regex.Matches(line, "Content-Transfer-Encoding:\s*(7bit|8bit|binary|base64|quoted_printable).*") with
-      | mc when 0 < mc.Count -> mc[0].Groups[1].Value
-      | _ -> f (i+1)
+      if lines.Length <= i 
+        then raise (exn "ContentTransferEncode is not found")
+        else
+          let line = lines[i]
+          match Regex.Matches(line, "Content-Transfer-Encoding:\s*(7bit|8bit|binary|base64|quoted_printable).*") with
+          | mc when 0 < mc.Count -> mc[0].Groups[1].Value
+          | _ -> f (i+1)
     f 0
 
+  let get_mime (lines: string[]) =
+    let rec f i =
+      if lines.Length <= i 
+        then raise (exn "MIME-type is not found")
+        else
+          let line = lines[i]
+          match Regex.Matches(line, "Content-Type:\s*((application|audio|example|font|image|model|text|video|message|multipart)/([a-zA-Z-]*)).*") with
+          | mc when 0 < mc.Count -> mc[0].Groups[2].Value
+          | _ -> f (i+1)
+    f 0
 
+  let get_charset (lines: string[]) =
+    let rec f i =
+      if lines.Length <= i 
+        then Encoding.UTF8
+        else
+          let line = lines[i]
+          match Regex.Matches(line, "Content-Type:.*?charset=\"(.*?)\".*") with
+          | mc when 0 < mc.Count -> Encoding.GetEncoding(mc[0].Groups[1].Value)
+          | _ -> f (i+1)
+    f 0
 
   let read (MhtFile file) =
     file.Reset()
